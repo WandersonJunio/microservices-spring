fbf6966ba74557075d10e3ea0abc35d67fca1c2b
WandersonJunio <wanderson.junioantunes@outlook.com>
2021-09-23 21:50:43 -0300

    Atualizacao de dependencias

---
+++ src/test/java/com/productcatalog/productcatolg/ProductCatolgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productcatalog.productcatolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatolgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

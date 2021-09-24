99cb3dc97a6f523d945ffffe7ef0cf2b45f39f02
WandersonJunio <wanderson.junioantunes@outlook.com>
2021-09-23 21:33:07 -0300

    Criando projeto

---
+++ product-catolg/product-catolg/src/main/java/com/productcatalog/productcatolg/ProductCatolgApplication.java
@@ -0,0 +1,13 @@
+package com.productcatalog.productcatolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatolgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatolgApplication.class, args);
+	}
+
+}
